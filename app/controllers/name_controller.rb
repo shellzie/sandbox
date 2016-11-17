3cec19544c980a61b131b7f4f8a271756067b0ab
dev <michellekam@gmail.com>
2016-11-17 10:36:58 -0700

    added database and updated params in db.yml

--- app/controllers/name_controller.rb
+++ app/controllers/name_controller.rb
@@ -2,7 +2,7 @@ class NameController < ApplicationController
 
   def michelle
     logger.debug "+++++++++++ in name controller, name function ++++++"
-    render :nothing => true, status => 200
+    render 'michelle'
   end
 
 
